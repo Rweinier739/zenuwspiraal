9cbb408ef915d3d00dee79ba6c4f494a3577eaf4
Rweinier739 <100585446+Rweinier739@users.noreply.github.com>
2022-05-23 14:36:06 +0200

    notting

--- main.ts
+++ main.ts
@@ -1,3 +1,17 @@
+input.onPinPressed(TouchPin.P1, function () {
+    basic.showIcon(IconNames.Sad)
+    music.startMelody(music.builtInMelody(Melodies.Wawawawaa), MelodyOptions.Once)
+    punten += -1
+    basic.showNumber(punten)
+})
+let punten = 0
+punten = 10
+music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
 basic.forever(function () {
-	
+    while (punten > 0) {
+        basic.showIcon(IconNames.Happy)
+    }
+    basic.showIcon(IconNames.No)
+    music.startMelody(music.builtInMelody(Melodies.PowerDown), MelodyOptions.Once)
+    basic.showString("Game over")
 })
